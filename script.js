7ccbd231f8df6199b7d3960af3ba24c4fc53d964
juanfelipeghost <juanfgracias@gmail.com>
2025-05-02 21:19:23 -0500

    ejemplo destruturing

--- script.js
+++ script.js
@@ -145,3 +145,25 @@ function getBook(id) {
 const books= getBooks();
 books;
 
+const book = getBook(3);
+book;
+/*const title = book.title;
+title;
+const author = book.author;
+author;
+*/
+// destructuring
+const { title, author ,genres} = book;
+console.log(title, author,genres);
+//const primaryGenre = genres[1];
+//primaryGenre;
+const [primaryGenre,...otherGenres] = genres;
+console.log(primaryGenre,otherGenres);
+const newGenres = ["epic fantasy",...genres];
+newGenres;
+const updatedBook = {
+  ...book,
+  moviePublicationDate: "2001-12-19",
+  pages: 1210,
+};
+updatedBook;
